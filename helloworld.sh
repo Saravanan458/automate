9911f9978ba0d12766349194fa039eed94585c95
Saravanan <saravananca458@gmail.com>
2020-08-20 07:40:52 +0000

    Added new Features to Feature 1

--- helloworld.sh
+++ helloworld.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#This shell script will loop for 5 times
 valid=true
 count=1
 while [ $valid ]
 do
-	echo "print $count"
+	echo $count
 	if [ $count -eq 5 ];
+     #add this part in feature1
 	then
 		break
 	fi
 	((count++))
-
 done
+:
